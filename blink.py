641b7dfaa562b0920417f9d137c7b3e61dd59fa4
Mark Carrington <frumarkcarrington@gmail.com>
2023-09-22 00:48:05 +0530

    using the esp32 internal temperature sensor' && git push

--- blink.py
+++
@@ -1,9 +0,0 @@
-import time
-
-def blink(pin):
-    pin.value(1)
-    print("The LED is on... \n")
-    time.sleep(1)
-    pin.value(0)
-    print("The LED is off.... \n")
-    time.sleep(1)
--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import machine
 import time
-pin = machine.Pin(2, machine.Pin.OUT)
+# pin = machine.Pin(2, machine.Pin.OUT)
 
 # a blink sketch for the es
+# while True:
+#     pin.value(1)
+#     print('pin is turning on...\n')
+#     time.sleep(1)
+#     pin.value(0)
+#     print('pin is going off\n\n')
+#     time.sleep(1)
+
+
+### this code can be used to read the internal temperature of the esp32 in fahrenheits
+# but T(f) = theta + 
+
+import esp32
+import time
+temp = esp32.raw_temperature()
+
 while True:
-    pin.value(1)
-    print('pin is turning on...\n')
-    time.sleep(1)
-    pin.value(0)
-    print('pin is going off\n\n')
+    print(f'The internal temperature in fahrenheit is: {temp}\n')
+    print(f'The internal temperature in degrees centigrades is {(temp-32)*(5/9)}\n')
     time.sleep(1)
